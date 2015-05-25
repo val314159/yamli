f753cb8d938b0002cd861c0622d055e045a182b1
val314159 <jmward@gmail.com>
2015-05-25 15:04:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 setup(name='yamli',version='0.2',py_modules=['yamli'],
 author='val314159',author_email='jmward@gmail.com',
-url='https://github.com/val314159/yamli')
+install_requires='pyaml',url='https://github.com/val314159/yamli')
