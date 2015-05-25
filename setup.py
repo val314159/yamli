13f8f1e0ddd4af1f15828598985e0b7e709bb55f
Joel Ward <jmward@@gmail.com>
2015-05-25 14:48:07 -0700

    update setup file

--- setup.py
+++ setup.py
@@ -1,2 +1,4 @@
 from distutils.core import setup
-setup(name='yamli',version='0.1',py_modules=['yamli'])
+setup(name='yamli',version='0.2',py_modules=['yamli'],
+author='val314159',author_email='jmward@gmail.com',
+url='https://github.com/val314159/yamli')
